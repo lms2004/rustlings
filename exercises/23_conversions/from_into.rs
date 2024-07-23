e78eefe8e6d0c8eba16693f00679a8d58dd894fb
刘铭澍 <mxylms1210@163.com>
2024-07-23 13:54:43 +0800

    finished ...

--- exercises/23_conversions/from_into.rs
+++ exercises/23_conversions/from_into.rs
@@ -34,7 +34,26 @@ impl Default for Person {
 // 5. Parse the second element from the split operation into a `u8` as the age.
 // 6. If parsing the age fails, return the default of `Person`.
 impl From<&str> for Person {
-    fn from(s: &str) -> Self {}
+    fn from(s: &str) -> Self {
+        let parts: Vec<&str> = s.split(',').collect();
+
+        if parts.len() != 2 {
+            return Person::default()
+        }
+        
+        let name = parts[0].trim().to_string();
+
+        if name.is_empty(){
+            return Person::default()
+        }
+
+        if let Ok(age) = parts[1].trim().parse::<u8>(){
+            Person{name, age}
+        }else{
+            Person::default()
+        }
+
+    }
 }
 
 fn main() {
--- exercises/23_conversions/try_from_into.rs
+++ exercises/23_conversions/try_from_into.rs
@@ -28,14 +28,27 @@ enum IntoColorError {
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
 
-    fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {}
+    fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        let (a, b, c) = tuple;
+        if a < 0 || a > 255 || b < 0 || b > 255 || c < 0 || c > 255 {
+            Err(IntoColorError::IntConversion)
+        }else{
+            Ok(Color {
+                red: a as u8,
+                green: b as u8,
+                blue: c as u8,
+            })
+        }
+    }
 }
 
 // TODO: Array implementation.
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
 
-    fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {}
+    fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        Self::try_from((arr[0], arr[1], arr[2]))
+    }
 }
 
 // TODO: Slice implementation.
@@ -43,7 +56,13 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
 
-    fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {}
+    fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3{
+            Err(IntoColorError::BadLen)
+        }else{
+            Self::try_from((slice[0], slice[1], slice[2]))
+        }
+    }
 }
 
 fn main() {
